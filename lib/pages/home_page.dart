9339fe6d1a045cbaa04827b8297ab3dd68562f82
keremilker46 <atolyebilgisayar46@gmail.com>
2026-03-05 11:30:40 +0300

    duzeltmeler

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,85 +10,100 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-
-
 class _HomePageState extends State<HomePage> {
-  
   bool fiyatGoster = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
-      
-      title: Text("${Veritabani.urunler.length} ürün var"), centerTitle: true,
-      actions: [
-        Text("Fiyat Göster"),
-        Checkbox(value: fiyatGoster, onChanged: (value){
-          setState(() {
-            fiyatGoster = value!;
-          });
-        })
-      ],
-      
+        title: Text("${Veritabani.urunler.length} ürün var", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Colors.cyan,
+        actions: [
+          Text("Fiyat Göster", style: TextStyle(color: Colors.white)),
+          Checkbox(
+            
+            value: fiyatGoster,
+            hoverColor: const Color.fromARGB(255, 176, 220, 255),
+            activeColor: const Color.fromARGB(255, 1, 49, 182),
+            checkColor: Colors.white,
+            focusColor: const Color.fromARGB(255, 0, 238, 255),
+            
+            onChanged: (value) {
+              setState(() {
+                fiyatGoster = value!;
+              });
+            },
+          ),
+        ],
       ),
-      
-      body: ListView.builder(itemBuilder: (context, index) {
-        Urun urun = Veritabani.urunler[index];
-        
-        if(fiyatGoster){
-          return GestureDetector(
-          onLongPress: () {
-            setState(() {
-              Veritabani.urunler.removeAt(index);
-            });
-          },
-          
-          child: Card(
-            
-            color: const Color.fromARGB(255, 175, 219, 255),
-            child: ListTile(
-              leading:  Icon(Icons.shopping_cart),
-              title: Text(urun.ad),
-              subtitle: Row(
-                children: [
-                  Text("${urun.fiyat} TL", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red),),
-                  Text(" ${urun.kategori}"),
-                ],
-              )  
-            ),
-          ),
-        );
-        }
-        else {
-          return GestureDetector(
-          onLongPress: () {
-            setState(() {
-              Veritabani.urunler.removeAt(index);
-            });
-          },
-          child: Card(
-            
-            color: const Color.fromARGB(255, 175, 219, 255),
-            child: ListTile(
-              leading: Icon(Icons.shopping_cart),
-              title: Text(urun.ad),
-              subtitle: Text(urun.kategori),
-            ),
-          ),
-        );
-        }
-      }, itemCount: Veritabani.urunler.length, shrinkWrap: true, physics: NeverScrollableScrollPhysics()),
+
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          Urun urun = Veritabani.urunler[index];
+
+          if (fiyatGoster) {
+            return GestureDetector(
+              onLongPress: () {
+                setState(() {
+                  Veritabani.urunler.removeAt(index);
+                });
+              },
+
+              child: Card(
+                color: const Color.fromARGB(255, 175, 219, 255),
+                child: ListTile(
+                  leading: Icon(Icons.shopping_cart, color: Colors.deepOrange),
+                  title: Text(
+                    urun.ad,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue),
+                  ),
+                  subtitle: Row(
+                    children: [
+                      Text(
+                        "${urun.fiyat} TL",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Text(" - "),
+                      Text(" ${urun.kategori}", style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return GestureDetector(
+              onLongPress: () {
+                setState(() {
+                  Veritabani.urunler.removeAt(index);
+                });
+              },
+              child: Card(
+                color: const Color.fromARGB(255, 175, 219, 255),
+                child: ListTile(
+                  leading: Icon(Icons.shopping_cart, color: Colors.deepOrange),
+                  title: Text(urun.ad, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue)),
+                  subtitle: Text(urun.kategori, style: TextStyle(color: Colors.black)),
+                ),
+              ),
+            );
+          }
+        },
+        itemCount: Veritabani.urunler.length,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+      ),
 
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-        await Navigator.push(
+          await Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => UrunlerPage()),
           );
-          setState(() {
-            
-          });
+          setState(() {});
         },
         child: Icon(Icons.add),
       ),
